8f81ef305415e2886e9913967e0e086964139a4d
Roopesh Sai <127601942+Roopesh2004@users.noreply.github.com>
2025-06-02 14:31:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 
